0586bf262ba64d5b7aef25fbecaab3dd27a0ae43
Thiago Gonçalves <goncalves.thiago@ctis.com.br>
2020-06-03 21:22:23 -0300

    Criação da estrutura básica do componente Home e criação da pasta assets para inclusão de imagens

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import './App.css';
 
-import Header from './Header';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div>
-      <Header />
-
-      <h1>Conteúdo da minha aplicação</h1>
+      <Home />
     </div>
   );
 }
